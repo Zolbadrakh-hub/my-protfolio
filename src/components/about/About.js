6a750329a4da40ea975cfd8ad7864aaa3549cf43
Zolbadrakh <zolbadrakh.a@devever.mn>
2022-06-30 11:49:12 +0800

    fix

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -26,7 +26,7 @@ const About = () => {
     <div className='bg-[#00688F] text-white py-28 px-20'>
             <h1 className='md:text-4xl text-2xl font-bold pb-4 border-b-8 border-[#00AEEF] md:w-72'>About Me...</h1>
             <p className='pt-16 md:text-lg text-sm'> Hello, My name is Zolbadrakh. </p>
-                <p className='md:text-lg text-sm'> I am a Software Enginer Undergraduate at National University of Mongolia. </p>
+                <p className='md:text-lg text-sm'> I am a Software Engineer Undergraduate at National University of Mongolia. </p>
                 <p className='md:text-lg text-sm pb-8'> I’m currently learning UI/UX development. Checkout my projects on github and behance. <a href='https://github.com/Zolbadrakh-hub' className='text-[#00AEEF]'>https://github.com/Zolbadrakh-hub</a> </p>
         </div>
     </>
